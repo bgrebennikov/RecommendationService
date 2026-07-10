3ec9c66aa44f7051be85e6b6b841a87111979702
boris <bgrebennikovv@gmail.com>
2026-07-10 19:48:46 +0400

    add empty class RecommendationsDataSourceConfiguration

---
+++ src/main/java/com/github/bgrebennikov/recommendationservice/config/RecommendationsDataSourceConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.bgrebennikov.recommendationservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RecommendationsDataSourceConfiguration {
+
+
+}
