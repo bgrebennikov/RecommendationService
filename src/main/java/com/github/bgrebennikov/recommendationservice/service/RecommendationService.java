33ed15c5499c7254c63a60e2e4038940d15c8b5d
user <ksodnoralov@yandex.ru>
2026-07-12 12:32:03 +0300

    Created Controller and Service. rev. A

---
+++ src/main/java/com/github/bgrebennikov/recommendationservice/service/RecommendationService.java
@@ -0,0 +1,31 @@
+package com.github.bgrebennikov.recommendationservice.service;
+
+import com.github.bgrebennikov.recommendationservice.data.RecommendationItem;
+import com.github.bgrebennikov.recommendationservice.data.RecommendationResponse;
+import com.github.bgrebennikov.recommendationservice.rule.RecommendationRuleSet;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class RecommendationService {
+
+    private final List<RecommendationRuleSet> ruleSets;
+
+    public RecommendationService(List<RecommendationRuleSet> ruleSets) {
+        this.ruleSets = ruleSets;
+    }
+
+    public RecommendationResponse getRecommendations(UUID userId) {
+        List<RecommendationItem> recommendations = ruleSets.stream()
+                .map(rule -> rule.evaluate(userId))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+        return new RecommendationResponse(userId.toString(), recommendations);
+    }
+}
