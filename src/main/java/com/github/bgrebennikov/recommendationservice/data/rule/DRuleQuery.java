a65c4da3c7874b48c6a914d927e8f226b58f9d51
boris <bgrebennikovv@gmail.com>
2026-07-17 18:12:10 +0400

    add DTO for dynamic rules

---
+++ src/main/java/com/github/bgrebennikov/recommendationservice/data/rule/DRuleQuery.java
@@ -0,0 +1,10 @@
+package com.github.bgrebennikov.recommendationservice.data.rule;
+
+public enum DRuleQuery{
+
+    USER_OF,
+    ACTIVE_USER_OF,
+    TRANSACTION_SUM_COMPARE,
+    TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW;
+
+}
