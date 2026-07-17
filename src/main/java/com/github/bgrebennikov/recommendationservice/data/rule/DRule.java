8d9f54e0a026dfa59a6dcada2da3442b1328c2f2
boris <bgrebennikovv@gmail.com>
2026-07-17 18:11:14 +0400

    add DTO for dynamic rules

---
+++ src/main/java/com/github/bgrebennikov/recommendationservice/data/rule/DRule.java
@@ -0,0 +1,40 @@
+package com.github.bgrebennikov.recommendationservice.data.rule;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+
+public class DRule {
+    @JsonProperty("query")
+    private DRuleQuery query;
+
+    @JsonProperty("arguments")
+    private List<String> arguments;
+
+    @JsonProperty("negate")
+    private Boolean negate;
+
+    DRule(DRuleQuery query, List<String> arguments, Boolean negate) {
+        this.query = query;
+        this.arguments = arguments;
+        this.negate = negate;
+    }
+
+    DRule() {
+    }
+
+    public List<String> getArguments() {
+        return arguments;
+    }
+
+    public DRuleQuery getQuery() {
+        return query;
+    }
+
+    public Boolean getNegate() {
+        return negate;
+    }
+
+
+}
