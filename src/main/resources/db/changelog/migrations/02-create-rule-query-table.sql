67380daf1198c52433a2f426284a50c67aba635f
boris <bgrebennikovv@gmail.com>
2026-07-22 12:09:19 +0400

    fix: liquibase headers & invalid migrations path

--- src/main/resources/db/changelog/migrations/02-create-rule-query-table.sql
+++ src/main/resources/db/changelog/migrations/02-create-rule-query-table.sql
@@ -1,9 +1,12 @@
-CREATE TABLE IF NOT EXISTS rule_query (
-    id UUID PRIMARY KEY,
-    rule_id UUID NOT NULL,
+--liquibase formatted sql
+--changeset boris:02-create-rule-query-table
+CREATE TABLE IF NOT EXISTS rule_query
+(
+    id         UUID PRIMARY KEY,
+    rule_id    UUID        NOT NULL,
     query_type VARCHAR(50) NOT NULL,
-    arguments JSONB,
-    negate BOOLEAN NOT NULL,
-    sort_order INT NOT NULL,
+    arguments  JSONB,
+    negate     BOOLEAN     NOT NULL,
+    sort_order INT         NOT NULL,
     CONSTRAINT fk_rule_query_rule FOREIGN KEY (rule_id) REFERENCES rule_entity (id) ON DELETE CASCADE
 );
