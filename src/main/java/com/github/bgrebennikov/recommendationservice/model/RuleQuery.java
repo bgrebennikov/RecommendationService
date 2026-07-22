1a2a885af1139a8a3dc1cff9a44d3fe7c02b8c1c
boris <bgrebennikovv@gmail.com>
2026-07-22 21:59:36 +0400

    refactor: add javaDoc description

--- src/main/java/com/github/bgrebennikov/recommendationservice/model/RuleQuery.java
+++ src/main/java/com/github/bgrebennikov/recommendationservice/model/RuleQuery.java
@@ -10,6 +10,15 @@ import org.hibernate.type.SqlTypes;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Сущность условия (запроса), входящего в состав динамического правила.
+ * <p>
+ * Определяет конкретную проверку (например, наличие продукта или сумма транзакций)
+ * и аргументы для выполнения этой проверки.
+ *
+ * @author Konstantin, Boris
+ * @version 1.0
+ */
 @Entity
 @Table(name = "rule_query")
 public class RuleQuery {
@@ -39,8 +48,15 @@ public class RuleQuery {
     private RuleEntity rule;
 
 
-    public RuleQuery() {}
+    public RuleQuery() {
+    }
 
+    /**
+     * @param queryType Тип запроса/проверки
+     * @param arguments Список аргументов для запроса
+     * @param negate    Флаг отрицания результата
+     * @param sortOrder Порядок сортировки условия
+     */
     public RuleQuery(DRuleQuery queryType, List<String> arguments, Boolean negate, Integer sortOrder) {
         this.queryType = queryType;
         this.arguments = arguments;
@@ -48,22 +64,52 @@ public class RuleQuery {
         this.sortOrder = sortOrder;
     }
 
-    public UUID getId() { return id; }
-    public void setId(UUID id) { this.id = id; }
+    public UUID getId() {
+        return id;
+    }
 
-    public DRuleQuery getQueryType() { return queryType; }
-    public void setQueryType(DRuleQuery queryType) { this.queryType = queryType; }
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
-    public List<String> getArguments() { return arguments; }
-    public void setArguments(List<String> arguments) { this.arguments = arguments; }
+    public DRuleQuery getQueryType() {
+        return queryType;
+    }
 
-    public Boolean getNegate() { return negate; }
-    public void setNegate(Boolean negate) { this.negate = negate; }
+    public void setQueryType(DRuleQuery queryType) {
+        this.queryType = queryType;
+    }
 
-    public Integer getSortOrder() { return sortOrder; }
-    public void setSortOrder(Integer sortOrder) { this.sortOrder = sortOrder; }
+    public List<String> getArguments() {
+        return arguments;
+    }
 
-    public RuleEntity getRule() { return rule; }
-    public void setRule(RuleEntity rule) { this.rule = rule; }
+    public void setArguments(List<String> arguments) {
+        this.arguments = arguments;
+    }
+
+    public Boolean getNegate() {
+        return negate;
+    }
+
+    public void setNegate(Boolean negate) {
+        this.negate = negate;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public RuleEntity getRule() {
+        return rule;
+    }
+
+    public void setRule(RuleEntity rule) {
+        this.rule = rule;
+    }
 }
 
