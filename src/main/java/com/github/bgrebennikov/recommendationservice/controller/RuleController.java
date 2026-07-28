1708c7d11c8e4ea4fcc4dd1105758f0e50125059
boris <bgrebennikovv@gmail.com>
2026-07-28 16:47:40 +0400

    ref: update javaDoc

--- src/main/java/com/github/bgrebennikov/recommendationservice/controller/RuleController.java
+++ src/main/java/com/github/bgrebennikov/recommendationservice/controller/RuleController.java
@@ -3,7 +3,6 @@ package com.github.bgrebennikov.recommendationservice.controller;
 import com.github.bgrebennikov.recommendationservice.data.dto.rule.RuleCreateRequest;
 import com.github.bgrebennikov.recommendationservice.data.dto.rule.RuleItemDto;
 import com.github.bgrebennikov.recommendationservice.data.dto.rule.RuleResponseListDto;
-import com.github.bgrebennikov.recommendationservice.data.persistence.RuleEntity;
 import com.github.bgrebennikov.recommendationservice.service.RuleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 /**
- * REST-контроллер для управления правилами рекомендаций.
+ * REST-контроллер для управления динамическими правилами рекомендаций.
  * <p>
- * Предоставляет HTTP-эндпоинты для создания, получения и удаления динамических правил.
+ * Предоставляет HTTP-эндпоинты для создания, получения и удаления правил.
  *
  * @author Ekaterina, Boris
- * @version 1.0
+ * @version 1.1
  */
 @RestController
 @RequestMapping("/rule")
@@ -29,10 +28,10 @@ public class RuleController {
     }
 
     /**
-     * Создает новое правило рекомендаций.
+     * Создает новое правило рекомендаций вместе с его условиями.
      *
-     * @param request DTO с параметрами создаваемого правила и его условиями
-     * @return Созданная сущность {@link RuleEntity} со статусом 200 OK
+     * @param request DTO с параметрами создаваемого правила и списком условий
+     * @return DTO созданного правила {@link RuleItemDto} со статусом 200 OK
      */
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
@@ -43,7 +42,7 @@ public class RuleController {
     /**
      * Возвращает полный список всех зарегистрированных правил.
      *
-     * @return Список сущностей {@link RuleResponseListDto} со статусом 200 OK
+     * @return DTO-контейнер {@link RuleResponseListDto} со списком правил и статусом 200 OK
      */
     @GetMapping
     public RuleResponseListDto getAllRules() {
