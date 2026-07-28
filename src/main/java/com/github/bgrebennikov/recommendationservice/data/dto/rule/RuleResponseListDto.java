f1e2a46e444a4a11aa0608680fc89a1ba18cb059
boris <bgrebennikovv@gmail.com>
2026-07-28 16:40:45 +0400

    ref: rename for separate models between db entity & DTO

--- src/main/java/com/github/bgrebennikov/recommendationservice/data/dto/rule/RuleResponseListDto.java
+++ src/main/java/com/github/bgrebennikov/recommendationservice/data/dto/rule/RuleResponseListDto.java
@@ -4,7 +4,7 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
-public record RuleResponseDto(
+public record RuleResponseListDto(
         List<RuleItemDto> data
 ) implements Serializable {
     @Serial
